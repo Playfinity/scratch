224773afe44f6016fb34da6561e139955eb20ea5
Stian Vælitalo <svel@>
2020-02-25 13:26:11 +0100

    Wip

--- ball.js
+++ ball.js
@@ -1,32 +1,6 @@
 importScripts("https://cloud01.playfinity.io/socket.io/socket.io.js");
 importScripts("https://code.jquery.com/jquery-1.11.1.js");
 
-(function(ext) {
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
-
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
-
-    ext.power = function(base, exponent) {
-        return Math.pow(base, exponent);
-    };
-
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            // Block type, block name, function name, param1 default value, param2 default value
-            ['r', '%n ^ %n', 'power', 2, 3],
-        ]
-    };
-
-    // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
-})({});
-
 new (function() {
     var ext = this;
 
@@ -51,10 +25,15 @@ new (function() {
         });
     };
 
+    ext.power = function(base, exponent) {
+        return Math.pow(base, exponent);
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['w', 'when ball catch occurs', 'ballCatch'],
+            ['r', '%n ^ %n', 'power', 2, 3],
         ]
     };
 
